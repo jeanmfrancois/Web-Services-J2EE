321f8039898fa1df5edb533df15c82730c41414a
Jean-francois Nepton <jeanmfrancois@gmail.com>
2014-08-07 12:43:50 -0400

    Finish Stock Data Challenge - Reverse WSDL

---
+++ Stock Data Challenge/src/com/jfbuilds/webservices/StockData.java
@@ -0,0 +1,13 @@
+package com.jfbuilds.webservices;
+
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+
+@WebService
+public class StockData {
+	
+		@WebMethod
+		public void constructor() {
+			
+		}
+}
